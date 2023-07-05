8ce49f581b9656fb0d65f53f09ea834ac4329592
Nate Maile <npmaile@gmail.com>
2023-07-04 21:38:01 -0400

    added windows stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/npmaile/papeChanger
 
 go 1.20
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221212164502-fae10dda9338 // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/tools v0.10.1-0.20230622221742-0622ad2359a7 // indirect
+	golang.org/x/tools/gopls v0.12.4 // indirect
+	golang.org/x/vuln v0.0.0-20230110180137-6ad3e3d07815 // indirect
+	honnef.co/go/tools v0.4.2 // indirect
+	mvdan.cc/gofumpt v0.4.0 // indirect
+	mvdan.cc/xurls/v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 	"math/rand"
 	"os"
 	"os/user"
-	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"time"
 
 	"github.com/npmaile/papeChanger/chooser"
 	"github.com/npmaile/papeChanger/papesetter"
 )
+
+func init() {
+	rand.Seed(int64(time.Now().Nanosecond()))
+}
 
 const errPrefix = "😭😭oOpSy DoOpSiE, you made a frickey-wickey 😭😭: "
 
@@ -21,19 +26,51 @@ func main() {
 	// parse command line arguments
 	changeDir := flag.Bool("c", false, "Change the directory you are selecting walpapers from")
 	randomize := flag.Bool("r", true, "Randomize wallpaper to change")
+	setup := flag.Bool("setup", false, "set walpaper for the first time")
 	u, err := user.Current()
 	if err != nil {
 		log.Fatalf("%sHow the H**K are you not logged in as a user?", errPrefix)
 	}
 	homeDir := u.HomeDir
-	stateFile := flag.String("m", path.Join(homeDir, ".local/papeChanger/state"), "Use a custom location to store the current walpaper set")
+	var stateFile *string
+	switch runtime.GOOS {
+	case "windows":
+		stateFile = flag.String("m", filepath.Join(homeDir, "AppData", "Local", "papeChanger", "state"), "Use a custom location to store the current walpaper set")
+	default:
+		stateFile = flag.String("m", filepath.Join(homeDir, ".local", "papeChanger", "state"), "Use a custom location to store the current walpaper set")
+	}
 	flag.Parse()
+
+	if *setup {
+		filepathraw := os.Args[len(os.Args)-1]
+		var papePath string
+		papePath, err := filepath.Abs(filepathraw)
+		if err != nil {
+			log.Fatalf("%sUnable to find file %s: %e", errPrefix, filepathraw, err)
+		}
+		log.Printf("Setting wallpaper to %s", papePath)
+		err = papesetter.SetPape(papePath)
+		if err != nil {
+			log.Fatalf("%sUnable to set walpaper to %s: %e", errPrefix, filepathraw, err)
+		}
+		err = writeState(*stateFile, papePath)
+		if err != nil {
+			log.Fatalf("%sUnable to write state file %s: %e", errPrefix, *stateFile, err)
+		}
+		os.Exit(0)
+	}
 
 	currentWalpaper, err := os.ReadFile(*stateFile)
 	if err != nil {
-		log.Fatalf("%sCan't read the file", errPrefix)
+		log.Fatalf("%sCan't read the file: %e", errPrefix, err)
 	}
-	pathParts := strings.Split(string(currentWalpaper), string(os.PathSeparator))
+	var pathParts []string
+	switch runtime.GOOS {
+	case "windows":
+		pathParts = strings.Split(string(currentWalpaper), string(os.PathSeparator))
+	default:
+		pathParts = strings.Split(string(currentWalpaper), string(os.PathSeparator))
+	}
 	currentDirParts := pathParts[0 : len(pathParts)-1]
 	if *changeDir {
 		megaDir := string(os.PathSeparator) + filepath.Join(currentDirParts[0:len(currentDirParts)-1]...)
@@ -55,11 +92,20 @@ func main() {
 		}
 		currentDirParts[len(currentDirParts)-1] = string(chosen)
 	}
-	walpaperFolder := string(os.PathSeparator) + filepath.Join(currentDirParts...)
+
+	var walpaperFolder string
+	switch runtime.GOOS {
+	case "windows":
+		folderParts := append([]string{currentDirParts[0], "\\"}, currentDirParts[1:]...)
+		walpaperFolder = string(filepath.Join(folderParts...))
+	default:
+		walpaperFolder = string(os.PathSeparator) + filepath.Join(currentDirParts...)
+	}
 	papers, err := os.ReadDir(walpaperFolder)
 	if err != nil {
 		log.Fatalf("%sUnable to get list of individual walpapers: %e", errPrefix, err)
 	}
+
 	var fullPath []string
 	if *randomize {
 		index := rand.Int() % len(papers)
@@ -67,14 +113,29 @@ func main() {
 	} else {
 		//todo
 	}
-	newWalpaper := string(os.PathSeparator) + filepath.Join(fullPath...)
+	var newWalpaper string
+	switch runtime.GOOS {
+	case "windows":
+		folderParts := append([]string{fullPath[0], "\\"}, fullPath[1:]...)
+		newWalpaper = string(filepath.Join(folderParts...))
+	default:
+		newWalpaper = string(os.PathSeparator) + filepath.Join(fullPath...)
+	}
 	err = papesetter.SetPape(newWalpaper)
 	if err != nil {
 		log.Fatalf("%sunable to change walpaper: %e", errPrefix, err)
 	}
-	f, err := os.Create(*stateFile)
+	err = writeState(*stateFile, newWalpaper)
 	if err != nil {
 		log.Fatalf("%sCreation of state file failed: %e", errPrefix, err)
 	}
-	f.Write([]byte(newWalpaper))
+}
+
+func writeState(stateFile string, newWalpaper string) error {
+	f, err := os.Create(stateFile)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write([]byte(newWalpaper))
+	return nil
 }
