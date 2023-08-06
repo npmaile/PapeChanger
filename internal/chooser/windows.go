561c8c22925e099a0dc20c9f0027120dfca0d57e
Nathaniel Maile <npmaile@gmail.com>
2023-08-06 01:58:42 -0400

    use builtin chooser for command line windows

--- internal/chooser/windows.go
+++ internal/chooser/windows.go
@@ -3,7 +3,16 @@
 
 package chooser
 
+import(
+	"github.com/npmaile/papeChanger/internal/ui"
+	"fyne.io/fyne/v2/app"
+)
+
 func Chooser(dirs []string) (string, error) {
 	// will replace with something else if I find a good chooser for windows
-	return BuiltIn(dirs)
+	app := app.New()
+	selectionChan := make(chan string)
+	ui.ChooserWindow(app, dirs, selectionChan)
+	selection := <- selectionChan
+	return selection, nil
 }
