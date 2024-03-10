9bac55059a3198ee404dcd4f65ccab3dc6f63956
Nathaniel Maile <npmaile@gmail.com>
2024-03-09 19:41:26 -0500

    attempt to add global shortcuts (doesn't seem to work)

--- AppleWrapepr/PapeChanger/PapeChanger/PapeChangerApp.swift
+++ AppleWrapepr/PapeChanger/PapeChanger/PapeChangerApp.swift
@@ -7,10 +7,10 @@
 
 import SwiftUI
 import Foundation
+import KeyboardShortcuts
 
 @main
 struct PapeChangerApp: App {
-    @State private var showPicker: Bool = false
     var body: some Scene {
         MenuBarExtra("PapeChanger", systemImage: "square.on.square.fill") {
             Button("Change Wallpaper", action: ChangePape)
@@ -19,7 +19,28 @@ struct PapeChangerApp: App {
             }
             Button("Pick Wallpaper", action: selectPape)
             Divider()
+            SettingsLink()
+            Divider()
             Button("Quit") { NSApplication.shared.terminate(nil) }
         }.menuBarExtraStyle(.menu)
+        Settings{
+                SettingsView()
+        
+        }
+    }
+}
+
+@MainActor
+final class AppState: ObservableObject {
+    init() {
+        KeyboardShortcuts.onKeyDown(for: .ChangePape, action: ChangePape)
+    }
+}
+
+struct SettingsView: View{
+    var body: some View {
+        Form{
+            KeyboardShortcuts.Recorder("Change Wallpaper:", name: .ChangePape)
+        }
     }
 }
