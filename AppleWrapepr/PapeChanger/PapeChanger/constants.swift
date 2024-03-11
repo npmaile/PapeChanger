67c2dbd414a0238a145c9ae1c71d3d3a22c3bd5f
Nathaniel P Maile <npmaile@gmail.com>
2024-03-11 02:54:58 -0400

    Merge pull request #33 from npmaile/mac-keybinds
    Mac keybind

---
+++ AppleWrapepr/PapeChanger/PapeChanger/constants.swift
@@ -0,0 +1,11 @@
+//
+//  constants.swift
+//  Pape Changer
+//
+//  Created by Nathaniel Maile on 3/9/24.
+//
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name{
+    static let ChangePape = Self("changePape")
+}
